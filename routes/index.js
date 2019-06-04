49dd1c74a6f8f53cce304605eb02e63a90d26990
kot_davinchi <ilya_t@maximail.org>
2019-06-04 19:15:51 +0500

    iteration

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,21 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
-        block: ['Ты','молодец!', 'у тебя', 'получилось!']
+        square1: 'Ты',
+        square2: 'молодец!',
+        square3: 'У тебя',
+        square4: 'получилось!',
+        square5: ['привет','как дела?','что делаешь'],
+        squares: [
+            ['Ты','молодец!'],
+            ['У тебя','получилось!']
+        ],
+        tracks: [
+            [0, 'muse','bliss'],
+            [1, 'asdf', '12345'],
+            [2, 'asdf', '1223345'],
+            [4, 'asdf', '1223424345'],
+        ]
     });
 });
 
