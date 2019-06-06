ba288b1f3521a6c55703735ee31789b7eea824c7
kot_davinchi <ilya_t@maximail.org>
2019-06-06 19:26:04 +0500

    новое задание

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+function random(n){return Math.floor(Math.random()*n)}
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    const enties = ["мандарин", "апельсин", "грузин", "карантин", "маркер", "гендер", "монитор", "взрыв"];
+
+
     res.render('index', {
-        square1: 'Ты',
-        square2: 'молодец!',
-        square3: 'У тебя',
-        square4: 'получилось!',
-        square5: ['привет','как дела?','что делаешь'],
-        squares: [
-            ['Ты','молодец!'],
-            ['У тебя','получилось!']
-        ],
-        tracks: [
-            [0, 'muse','bliss'],
-            [1, 'asdf', '12345'],
-            [2, 'asdf', '1223345'],
-            [4, 'asdf', '1223424345'],
-        ]
+        array : Array(random(30)).fill(1).map(()=>({num:random(10),name:enties[random(8)]}))
     });
 });
 
